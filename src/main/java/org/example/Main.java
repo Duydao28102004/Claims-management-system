9a0726bec5261d0b290d9ef3677853926567885b
Duydao28102004 <119027061+Duydao28102004@users.noreply.github.com>
2024-03-31 19:04:53 +0700

    data manager

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,21 @@ package org.example;
 
 import org.example.System.ClaimProcessManager;
 import org.example.System.Claim;
+import org.example.System.FileManagement;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         ClaimProcessManager claimProcessManager = new ClaimProcessManager();
-        Claim claim = claimProcessManager.addClaim();
-        System.out.print(claim.toString());
+        FileManagement fileManagement = new FileManagement();
+//        Claim claim = claimProcessManager.addClaim();
+//        ArrayList<Claim> claims = new ArrayList<>();
+//        claims.add(claim);
+//        fileManagement.fileWriter(claims, "claim.json");
+        ArrayList<Claim> claims = fileManagement.claimReader();
+        for (Claim c : claims) {
+            System.out.println(c.toString());
+        }
     }
 }
---
+++ src/main/java/org/example/System/FileManagement.java
@@ -0,0 +1,55 @@
+package org.example.System;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileManagement {
+    public void fileWriter(Object object, String fileName) {
+        String folderPath = "./data/";
+        File file = new File(folderPath, fileName);
+        if (file.exists()) {
+            System.out.println("The file exists in the folder.");
+        } else {
+            System.out.println("The file does not exist in the folder.");
+            System.out.println("Creating a new file...");
+            try {
+                file.createNewFile();
+                System.out.println("The file is created.");
+            } catch (Exception e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+        try (FileWriter writer = new FileWriter(folderPath + fileName, false)) {
+            // Write JSON string to file
+            writer.write(json);
+            System.out.println("Object written to file successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public ArrayList<Claim> claimReader() {
+        Gson gson = new Gson();
+        String folderPath = "./data/";
+        String fileName = "claim.json";
+        File folder = new File(folderPath);
+        File file = new File(folderPath + fileName);
+        try (FileReader reader = new FileReader(folderPath + fileName)) {
+            // Read JSON file
+            TypeToken<ArrayList<Claim>> collectionType = new TypeToken<ArrayList<Claim>>(){};
+            ArrayList<Claim> claims = gson.fromJson(reader, collectionType);
+            System.out.println("Object read from file successfully!");
+            return claims;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
