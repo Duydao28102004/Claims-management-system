1e1e46bb8039145e6b86668fa30396a5aedafa5b
TungTran159 <s3978481@rmit.edu.vn>
2024-04-29 17:11:49 +0700

    Add Provider & InsuranceSurveyor

---
+++ src/main/java/org/example/System/Provider/InsuranceSurveyor.java
@@ -0,0 +1,13 @@
+package org.example.System.Provider;
+
+public class InsuranceSurveyor extends Provider{
+    public InsuranceSurveyor() {
+        super();
+    }
+
+    public InsuranceSurveyor(String id, String fullName, Number phone, String address, String email, String password) {
+        super(id, fullName, phone, address, email, password);
+    }
+
+
+}
---
+++ src/main/java/org/example/System/Provider/Provider.java
@@ -0,0 +1,79 @@
+package org.example.System.Provider;
+
+import org.example.System.Claim;
+import org.example.System.ClaimProcessManager;
+import java.util.ArrayList;
+
+public class Provider {
+
+    private String id;
+    private String fullName;
+    private Number phone;
+    private String address;
+    private String email;
+    private String password;
+
+    public Provider() {
+        this.id = "default";
+        this.fullName = "default";
+        this.phone = null;
+        this.address = null;
+        this.email = null;
+        this.password = null;
+    }
+
+    public Provider(String id, String fullName, Number phone, String address, String email, String password) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Number getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Number phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void printAllClaims(ArrayList<Claim> claims) {
+        ClaimProcessManager claimProcessManager = new ClaimProcessManager();
+        claimProcessManager.printClaim(claims);
+    }
+
+}
